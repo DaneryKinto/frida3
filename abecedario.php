42895c28a1af1f4e3db8f75d7549ef4461e89e52
Daviruz <dariomc2009@gmail.com>
2015-04-26 23:56:20 -0300

    Cambie diccionario por abecedario y categorias, diccionario vendria a ser un "boton".

---
+++ abecedario.php
@@ -0,0 +1,8 @@
+<?php
+		//CABECERA incluye el head (el css js), el nav. 
+			include("php/partes/header.php");
+		?>
+<!-- Este es el pie de pagina -->
+		<?php
+			include("php/partes/footer.php");
+		?>
