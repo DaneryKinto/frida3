04faf8667e7d08a633ec956c97450f2d67febe0b
Daviruz <dariomc2009@gmail.com>
2015-04-26 22:23:34 -0300

    Lo que olvido moncho y el nuevo menu XD

--- php/partes/footer.php
+++ php/partes/footer.php
@@ -1,13 +1,13 @@
 <div class="container-fluid">
-	<!-- Javascript -->
-	<script src="js/jquery.min.js" type="text/Javascript"></script>
-	<script src="js/code.js" type="text/Javascript"></script>
 	<footer class="row">
 		<div class="col-lg-12 col-md-12">
 			<h3>PIE DE PAGINA</h3>
 		</div>
 	</footer>		
 </div>
-	
+	<!-- Javascript -->
+	<script src="js/jquery.min.js" type="text/Javascript"></script>
+	<script src="js/code.js" type="text/Javascript"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- php/partes/header.php
+++ php/partes/header.php
@@ -19,42 +19,42 @@
 </head>
 <body>
 		
-			<nav class="navbar navbar-default row" role="navigation">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Comunidad</a>
-				</div>
-			
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="index.php">Inicio</a></li>
-						<li><a href="servicios.php">Servicios</a></li>
-						<li><a href="noticias.php">Noticas</a></li>
-						<li><a href="admin.php">Administrador</a></li>
-						<li class="dropdown">
-				          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Diccionarios <span class="caret"></span></a>
-				          <ul class="dropdown-menu" role="menu">
-				            <li><a href="diccionario.php">Por abedario</a></li>
-				            <li><a href="categoria.php">Por categoría</a></li>
-				          </ul>
-				        </li>
-					</ul>
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#admin">administrar</a></li>
-						
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</nav>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Brand</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Inicio<span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Servicios</a></li>
+	        <li><a href="#">Noticias</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu" role="menu">
+	            <li><a href="#">Por abecedario</a></li>
+	            <li><a href="#">Por categoría</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	      <form class="navbar-form navbar-left" role="search">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Search">
+	        </div>
+	        <button type="submit" class="btn btn-default">Submit</button>
+	      </form>
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">Administrar</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>		
