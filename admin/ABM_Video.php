063c16651c5b8d9f056f447dd988c4f1c42021fe
Moncho Lamas <moncholamas@gmail.com>
2015-05-19 14:08:22 -0300

    subir video comentarios de lo que falta

--- admin/ABM_Video.php
+++ admin/ABM_Video.php
@@ -18,12 +18,16 @@
     $direccion = "../img/subidas/$nombre";
     $direccion = "../img/$nombre";
 
+    //FALTA COMPROBAR QUE EL NOMBRE DEL VIDEO Y LA URL NO SE REPITAN
+
     // Comprobamos que el tamaño de imagen no pase de los 400kb, 409600 bytes.
     if ($tamanioimg > 409600){
         echo "Tu imagen debe ser menor a 400kb";
+        //debe volver a la pagina nuevo-video, error "el archivo es muy grande"
         $imagensubida = "false";
     } // Comprobamos el formato de imagenes permitidas.
     	else if (!( $tipoimg == "image/jpg" OR $tipoimg == "image/gif" OR $tipoimg == "image/jpeg" OR $tipoimg == "image/png")){
+            //debe volver a la pagina nuevo video, error "el formato no es compatible"
         	echo "Solo se permiten imágenes jpg o gif.";
     		$imagensubida = "false";
     	}  // Si la imagen paso las pruebas. 
@@ -36,10 +40,9 @@
 
             		$consulta = "INSERT INTO paginas (fecha, palabra, imagen, alt, video, d_video, categoria) VALUES ('$fecha', '$palabra', '$nombre', '$alt', '$url[1]', '$dvideo', '$categoria')";
             		$resultado = mysqli_query($conexion, $consulta);
-                   // print_r($conexion);
-                    print_r($resultado);
-            		//Si ocurre un error en la consulta.
-            		if(!isset($resultado)){ 
+
+                    if(!isset($resultado)){ 
+                   		//Si ocurre un error en la consulta, error "no se puedo modificar la base de datos"
                 		echo "<div class='alert alert-warning text-center' role='alert'>Error al subir el archivo 1 Vuelve a intentar<br> 
                  		<span class='label label-warning'><a href='../admin/subirvideo.php'>Regresar</a></span></div>";
             		} else{
